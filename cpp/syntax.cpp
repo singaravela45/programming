16a2a0a11df695782262d4e175747eb07edbbc29
singaravela45 <singaravelaramesh@gmail.com>
2026-02-20 10:13:20 +0530

    comment

--- cpp/syntax.cpp
+++ cpp/syntax.cpp
@@ -1,1 +1,37 @@
 //comments
+// Vector
+vector<int> v(n, 0);
+v.push_back(x);
+v.pop_back();
+v.size();
+v.clear();
+v.empty();
+v.front();
+v.back();
+v.begin();
+v.end();
+sort(v.begin(), v.end());
+reverse(v.begin(), v.end());
+
+//string 
+string s = "hello";
+s.length();
+s.size();
+s.substr(pos, len);
+s.find("abc");
+s.push_back('a');
+s.pop_back();
+reverse(s.begin(), s.end());
+
+// pairs
+pair<int,int> p = {1,2};
+p.first;
+p.second;
+
+//Stack
+stack<int> st;
+st.push(x);
+st.pop();
+st.top();
+st.empty();
+st.size();
