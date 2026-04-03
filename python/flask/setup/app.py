74d126d9b41583885b5dccbd8b23cf11e9deb74e
singaravela45 <singaravelaramesh@gmail.com>
2026-04-03 22:33:59 +0530

    comment

--- python/flask/setup/app.py
+++ python/flask/setup/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template
+import flask_mysqldb from MySQL
 app=Flask(__name__)
-@app.route("/")
+@app.route("/home")
 def index():
     return render_template("base.html")
 if __name__ == "__main__":
