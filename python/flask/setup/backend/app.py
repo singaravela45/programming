582651a9f2a97939d5bd7ec21e63b14e8fd1dcce
singaravela45 <singaravelaramesh@gmail.com>
2026-04-08 23:46:14 +0530

    comment

--- python/flask/setup/backend/app.py
+++ python/flask/setup/backend/app.py
@@ -63,4 +63,4 @@ def edit(user_id:int):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
